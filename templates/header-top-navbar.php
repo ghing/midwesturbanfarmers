b079065641a4abc80c917d7046b0756fee7fbd1e
Geoffrey Hing <geoff@terrorware.com>
2013-01-23 13:11:20 -0600

    Added news link to internal page navbar

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -10,6 +10,9 @@
         <?php bloginfo('name'); ?>
       </a>
       <nav id="nav-main" class="nav-collapse" role="navigation">
+        <ul class="nav">
+          <li><a href="<?php echo home_url(); ?>/#news">News</a></li>
+        </ul>
         <?php
           if (has_nav_menu('primary_navigation')) :
             wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav'));
